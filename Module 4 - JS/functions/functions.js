03c0e76cb198f50fc19340c0454edee3e8aeb349
crossfire3001 <obiwan2035@mail.ru>
2023-08-24 19:45:58 +0500

    working with functions

--- Module 4 - JS/functions/functions.js
+++ Module 4 - JS/functions/functions.js
@@ -61,3 +61,120 @@ let os = function(a, b) {
     return a + b;
 } // В отличие от обычной функции ее можно вызывать только после объявления
 
+/* Функция в составе объекта */
+let object = {
+    multiply: function(a, b) { // анонимная функция
+        return a * b;
+    }
+}
+
+console.log(object.multiply(3, 6));
+
+/* Использование анонимных функций */
+
+function func(param) {
+    param();
+}
+
+func(function () {
+    console.log('1'); // передача анонимной функции
+});
+
+/* Преждевременное использование return сразу завершает функцию */
+
+function f1(a, b) {
+    let sum = a + b;
+    return;
+    // тут код уже не выполнится
+}
+
+// пример 2
+let obj = {
+    sumTwo: function(a, b) {
+        if(a === 0) {
+            return null;
+        }
+        return a + b;
+    }
+}
+
+let resTwo = obj.sumTwo(1, 3);
+console.log(resTwo); // 4
+let resThree = obj.sumTwo(0, 3);
+console.log(resThree); // null
+
+/* Области видимости функций */
+
+function f2(b) {
+    let a = 1;
+}
+
+// console.log(a); // ошибка
+
+/* Правильная функция */
+
+function whatToDo(weather) {
+    if (weather) {
+        switch (weather) {
+            case 'дождь':
+                return 'Взять зонт!';
+            case 'град':
+                return 'Одеть шапку!';
+            }
+        } else {
+            return 'Невозможно определить погоду';
+        }
+}
+
+whatToDo('дождь');
+whatToDo('град');
+
+console.log('Если будет дождь, то я буду ' + whatToDo('дождь') + ', ' +
+    'но если будет град, то я буду ' + whatToDo('град'));
+
+/* Использование объектов */
+
+let person = {
+    name: 'Alex',
+    age: 20
+}
+
+let person2 = {
+    name: 'Sofia'
+}
+
+
+function checkAge(object) {
+    if (!object.age) {
+        return;
+    }
+    if (object.age >= 18 && object.age < 30) {
+        console.log('Возраст подходит');
+    } if (object.age !== 25) {
+        console.log('Не четверть века');
+    }
+}
+
+checkAge(person);
+checkAge(person2);
+checkAge({name: 'bobby', age: 29});
+
+// пример 2
+
+function checkAgeTwo(age) {
+    if (!age) {
+        return;
+    }
+    if (age >= 18 && age < 30) {
+        console.log('Возраст подходит');
+    } if (age !== 25) {
+        console.log('Не четверть века');
+    }
+
+}
+
+checkAgeTwo(person.age);
+checkAgeTwo(person2.age);
+checkAgeTwo(29);
+
+
